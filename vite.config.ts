34069959a4d586aebc0c3903018d31244e644ef7
고승범 <rhtmdqja4262@naver.com>
2025-11-05 01:27:31 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'bunyang',
+  base:'/bunyang/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
